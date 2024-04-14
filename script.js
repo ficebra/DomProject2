ab8e05819665146e22e3b9bec124bc3cd3f2df85
unknown <ossejoel@gmail.com>
2024-04-14 20:26:55 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    var cb = document.getElementById("color-box");
+    var cc = document.getElementById("change-color-btn");
+    console.log("DOM charge");
+});
+
+function getRandomColor(){
+    
+}
