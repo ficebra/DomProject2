1c457daf0e5508456ba989285c478f147e689ce1
unknown <ossejoel@gmail.com>
2024-04-15 22:33:41 +0000

    DOM Project 2 commited at 15042024

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+    
     var cb = document.getElementById("color-box");
     var cc = document.getElementById("change-color-btn");
+
+    function getRandomColor(){
+
+        let maxVal = 0xFFFFFF;
+        let randomNumber = Math.random() * maxVal;
+        randomNumber = Math.floor(randomNumber);
+        let randColor = randomNumber.toString(16);
+        cb.setAttribute("style", "background-color:#"+randColor);
+        return randColor;
+    }
+
+    cc.addEventListener("click", 
+    
+        function(){
+            getRandomColor();
+        },
+    );
     console.log("DOM charge");
 });
-
-function getRandomColor(){
-    
-}
